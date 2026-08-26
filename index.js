8dbb374668edf33f126b3f67c85e8039c5ede08c
mhnhai <maihangocha@gmail.com>
2026-08-26 20:32:12 +0700

    feat: add route health

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.get('/', (req, res) => {
   res.send('Chao mung den voi ung dung Node.js!');
 });
 
+app.get('/health', (req, res) => res.json({ ok: true }))
+
 // Route mới, chỉ tồn tại trên branch feature/about
 app.get('/about', (req, res) => {
   res.send('Day la trang About.');
