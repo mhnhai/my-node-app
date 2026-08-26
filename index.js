6de2b8fc59648dc43f497dae25e8c09f8d6ad354
mhnhai <maihangocha@gmail.com>
2026-08-26 19:43:10 +0700

    feat: đổi thông điệp chào ở route gốc

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-  res.end('Xin chao tu Git repository dau tien!\n');
+  res.end('Xin chao tu Git repository dau tien ello, GitHub!\n');
 });
 
 server.listen(3000, () => {
